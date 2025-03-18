3eb35a12cfbce275526a767604a85a4f65bf25ad
Heitor Marques Simões Barbosa <heitor.barbosa@santodigital.com.br>
2025-03-18 14:55:36 -0300

    feat: enhance email functionality to support HTML formatting for invite and password reset emails

--- project/app/utils/send_email.py
+++ project/app/utils/send_email.py
@@ -1,6 +1,9 @@
 from fastapi import BackgroundTasks
 from smtplib import SMTP
 import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 smtp_server = os.getenv("SMTP_SERVER")
 smtp_port = int(os.getenv("SMTP_PORT", 587))
@@ -9,47 +12,54 @@ smtp_password = os.getenv("SMTP_PASSWORD")
 
 def send_invite_email(email: str, invite_link: str):
     subject = "Convite para completar seu cadastro"
-    body = f"""
-    Olá,
     
-    Você foi convidado a acessar nossa plataforma. Para concluir seu cadastro, clique no link abaixo:
-
-    {invite_link}
-
-    Este link expira em 24 horas.
-
-    Atenciosamente,
-    Equipe do Sistema.
+    body = f"""
+    <html>
+    <body>
+        <p>Olá,</p>
+        <p>Você foi convidado a acessar nossa plataforma. Para concluir seu cadastro, clique no link abaixo:</p>
+        <p><a href="{invite_link}" style="font-size: 16px; color: blue; text-decoration: underline;">Completar Cadastro</a></p>
+        <p>Este link expira em 24 horas.</p>
+        <p>Atenciosamente,<br>Equipe do Sistema.</p>
+    </body>
+    </html>
     """
 
     print("LINK TOKEN", invite_link)
-    send_email(email, subject, body)
+    send_email(email, subject, body, html=True)
 
 def send_reset_password_email(email: str, reset_link: str):
     subject = "Redefinição de Senha"
+    
     body = f"""
-    Olá,
-
-    Recebemos uma solicitação para redefinir sua senha. Para criar uma nova senha, clique no link abaixo:
-
-    {reset_link}
-
-    Se você não solicitou esta redefinição, ignore este e-mail.
-
-    Atenciosamente,
-    Equipe do Sistema.
+    <html>
+    <body>
+        <p>Olá,</p>
+        <p>Recebemos uma solicitação para redefinir sua senha. Para criar uma nova senha, clique no link abaixo:</p>
+        <p><a href="{reset_link}" style="font-size: 16px; color: blue; text-decoration: underline;">Redefinir Senha</a></p>
+        <p>Se você não solicitou esta redefinição, ignore este e-mail.</p>
+        <p>Atenciosamente,<br>Equipe do Sistema.</p>
+    </body>
+    </html>
     """
 
     print("LINK TOKEN", reset_link)
-    send_email(email, subject, body)
+    send_email(email, subject, body, html=True)
 
 
-def send_email(to_email, subject, body):
-    message = f"Subject: {subject}\n\n{body}"
-    # to utf-8
-    message = message.encode('utf-8')
+def send_email(to_email, subject, body, html=False):
+    msg = MIMEMultipart()
+    msg["From"] = smtp_username
+    msg["To"] = to_email
+    msg["Subject"] = subject
+
+    # Definir o tipo de conteúdo como HTML ou texto simples
+    if html:
+        msg.attach(MIMEText(body, "html"))
+    else:
+        msg.attach(MIMEText(body, "plain"))
 
     with SMTP(smtp_server, smtp_port) as smtp:
         smtp.starttls()
         smtp.login(smtp_username, smtp_password)
-        smtp.sendmail(smtp_username, to_email, message)
+        smtp.sendmail(smtp_username, to_email, msg.as_string())
